3a7f33e794248fa697dce5b28ded51b3d5ad58cf
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-15 19:33:42 +0600

    push

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA0dwQTBxQBijXlHo93VC6UWio-U-RHS4E",
+  authDomain: "signin-recap.firebaseapp.com",
+  projectId: "signin-recap",
+  storageBucket: "signin-recap.appspot.com",
+  messagingSenderId: "296995436616",
+  appId: "1:296995436616:web:9b4091d04473fdab07c07d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
