a14d84770eea05518517afe8c8ed0676fdea979f
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-15 19:53:36 +0600

    push

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ function App() {
     const password = event.target.password.value
     console.log(email, password)
   }
-  const handleEmailChange = (event) =>{
+  const handleEmailBlur = (event) =>{
     console.log(event.target.value)
   }
-  const handlePasswordChange = (event) =>{
+  const handlePasswordBlur = (event) =>{
     console.log(event.target.value)
   }
 
   return (
     <div className="App">
      <form onSubmit={handleRegister}>
-      <input onChange={handleEmailChange} type="email" name="email" id="" placeholder='Your Email'  />
+      <input onBlur={handleEmailBlur} type="email" name="email" id="" placeholder='Your Email'  />
       <br />
-      <input onChange={handlePasswordChange} type="password" name="password" id="" placeholder='Your Password' />
+      <input onBlur={handlePasswordBlur} type="password" name="password" id="" placeholder='Your Password' />
       <br />
       <button type="submit">Register</button>
      </form>
